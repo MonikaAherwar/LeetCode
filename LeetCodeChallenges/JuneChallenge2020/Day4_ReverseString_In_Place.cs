af9d772b26b3f522ba83d2cb99999147a35220f5
MonikaAherwar <monikaaherwar@gmail.com>
2020-06-04 16:23:13 +0530

    Day4_ReverseString_In_Place

---
+++ LeetCodeChallenges/JuneChallenge2020/Day4_ReverseString_In_Place.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeChallenges.JuneChallenge2020
+{
+    class Day4_ReverseString_In_Place
+    {
+        public void ReverseString(char[] s)
+        {
+            var len = s.Length;
+            for (var i = 0; i < len / 2; i++)
+            {
+                var temp = s[i];
+                s[i] = s[len - i - 1];
+                s[len - i - 1] = temp;
+            }
+        }
+    }
+}
