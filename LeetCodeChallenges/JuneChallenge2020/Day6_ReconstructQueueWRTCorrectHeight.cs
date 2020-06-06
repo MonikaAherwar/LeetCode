053d23c2e66e2561c2f4653e31d23cb88c4cc378
MonikaAherwar <monikaaherwar@gmail.com>
2020-06-06 23:31:37 +0530

    Day6: Reconstruct Queue WRT Correct Height

---
+++ LeetCodeChallenges/JuneChallenge2020/Day6_ReconstructQueueWRTCorrectHeight.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+
+namespace LeetCodeChallenges.JuneChallenge2020
+{
+    class Day6_ReconstructQueueWRTCorrectHeight
+    {
+        public int[][] ReconstructQueue(int[][] people)
+        {
+            var map = Enumerable.Range(0, people.Length).ToList();
+            foreach (var person in people.OrderBy(p => p[0]).ThenByDescending(p => p[1]))
+            {
+                people[map[person[1]]] = new[] { person[0], person[1] };
+                map.RemoveAt(person[1]);
+            }
+
+            return people;
+        }
+    }
+}
