3c4c0bc33e3acc1017f79b223a32af882d1cb9c4
MonikaAherwar <monikaaherwar@gmail.com>
2020-06-07 23:34:29 +0530

    Day7: Coin Change Problem

---
+++ LeetCodeChallenges/JuneChallenge2020/Day7_CoinChangeProblem.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCodeChallenges.JuneChallenge2020
+{
+    class Day7_CoinChangeProblem
+    {
+        public int CoinChange(int amount, int[] coins)
+        {
+            int n = coins.Length;
+            int[] table = new int[amount + 1];
+
+            for (int i = 0; i < table.Length; i++)
+            {
+                table[i] = 0;
+            }
+
+            // Base case (If given value is 0) 
+            table[0] = 1;
+
+            for (int i = 0; i < n; i++)
+            for (int j = coins[i]; j <= amount; j++)
+                table[j] += table[j - coins[i]];
+
+            return table[amount];
+        }
+    }
+}
