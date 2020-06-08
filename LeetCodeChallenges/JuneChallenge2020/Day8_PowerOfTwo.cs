afecf228309f7be035ed0b3159a894fe8f3a905f
MonikaAherwar <monikaaherwar@gmail.com>
2020-06-09 00:20:36 +0530

    Day8: Power Of Two

---
+++ LeetCodeChallenges/JuneChallenge2020/Day8_PowerOfTwo.cs
@@ -0,0 +1,10 @@
+﻿namespace LeetCodeChallenges.JuneChallenge2020
+{
+    class Day8_PowerOfTwo
+    {
+        public bool IsPowerOfTwo(int x)
+        {
+            return !(x < 0) && x != 0 && ((x & (x - 1)) == 0);
+        }
+    }
+}
