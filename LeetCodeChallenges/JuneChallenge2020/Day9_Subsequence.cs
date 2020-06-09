601b19aa709f75a5945062524d14076963bed93c
MonikaAherwar <monikaaherwar@gmail.com>
2020-06-10 00:00:13 +0530

    Day9 Subsequence

---
+++ LeetCodeChallenges/JuneChallenge2020/Day9_Subsequence.cs
@@ -0,0 +1,29 @@
+﻿namespace LeetCodeChallenges.JuneChallenge2020
+{
+    class Day9_Subsequence
+    {
+        public bool IsSubsequence(string s, string t)
+        {
+            var m = s.Length;
+            var n = t.Length;
+
+            if (m == 0)
+            {
+                return true;
+            }
+
+            var j = 0;
+            for (var i = 0; i < n; i++)
+            {
+                if (t[i] == s[j])
+                {
+                    j++;
+
+                    if (j == m) return true;
+                }
+            }
+
+            return (j == m);
+        }
+    }
+}
