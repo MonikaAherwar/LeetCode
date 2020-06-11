552f100ab58b9ce3ba0704905b7c4c226d793b5e
MonikaAherwar <monikaaherwar@gmail.com>
2020-06-11 22:07:51 +0530

    Day 11: Sort Three Colors, Sort an array of (0, 1, 2)

---
+++ LeetCodeChallenges/JuneChallenge2020/Day11_SortThreeColors_0_1_2_Array.cs
@@ -0,0 +1,39 @@
+﻿namespace LeetCodeChallenges.JuneChallenge2020
+{
+    class Day11_SortThreeColors_0_1_2_Array
+    {
+        public void SortColors(int[] nums)
+        {
+            int lo = 0;
+            int hi = nums.Length - 1;
+            int mid = 0;
+
+            while (mid <= hi)
+            {
+                switch (nums[mid])
+                {
+                    case 0:
+                        {
+                            var temp = nums[lo];
+                            nums[lo] = nums[mid];
+                            nums[mid] = temp;
+                            lo++;
+                            mid++;
+                            break;
+                        }
+                    case 1:
+                        mid++;
+                        break;
+                    case 2:
+                        {
+                            var temp = nums[mid];
+                            nums[mid] = nums[hi];
+                            nums[hi] = temp;
+                            hi--;
+                            break;
+                        }
+                }
+            }
+        }
+    }
+}
