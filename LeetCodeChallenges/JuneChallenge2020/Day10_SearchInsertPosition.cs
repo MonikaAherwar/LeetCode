fb934a6d50a27a145f200aad3060c62bc26c1d67
MonikaAherwar <monikaaherwar@gmail.com>
2020-06-11 11:26:01 +0530

    Day10: Search Insert Position In Sorted Array

---
+++ LeetCodeChallenges/JuneChallenge2020/Day10_SearchInsertPosition.cs
@@ -0,0 +1,16 @@
+﻿namespace LeetCodeChallenges.JuneChallenge2020
+{
+    class Day10_SearchInsertPosition
+    {
+        public int SearchInsert(int[] nums, int target)
+        {
+            int i = 0;
+            while (i < nums.Length && nums[i] < target)
+            {
+                i++;
+            }
+
+            return i;
+        }
+    }
+}
