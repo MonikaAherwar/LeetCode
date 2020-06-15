a193db68a4e0cb47d8a0bb09d60eb9376399fe3d
MonikaAherwar <monikaaherwar@gmail.com>
2020-06-15 13:04:16 +0530

    Day13: Largest Divisible Subset problem

---
+++ LeetCodeChallenges/JuneChallenge2020/Day13_LargestDivisibleSubset.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCodeChallenges.JuneChallenge2020
+{
+    class Day13_LargestDivisibleSubset
+    {
+        public IList<int> LargestDivisibleSubset(int[] arr)
+        {
+
+            if (arr.Length == 0)
+            {
+                return new List<int>();
+            }
+
+            Array.Sort(arr);
+
+            int[] divCount = new int[arr.Length];
+
+            for (int i = 0; i < arr.Length; i++)
+                divCount[i] = 1;
+
+            int[] prev = new int[arr.Length];
+            for (int i = 0; i < arr.Length; i++)
+                prev[i] = -1;
+
+            int max_ind = 0;
+
+            for (int i = 1; i < arr.Length; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    if (arr[i] % arr[j] == 0 &&
+                        divCount[i] < divCount[j] + 1)
+                    {
+                        prev[i] = j;
+                        divCount[i] = divCount[j] + 1;
+
+                    }
+
+                }
+
+                if (divCount[i] > divCount[max_ind])
+                    max_ind = i;
+            }
+
+            int k = max_ind;
+            List<int> ans = new List<int>();
+            while (k >= 0)
+            {
+                ans.Add(arr[k]);
+                k = prev[k];
+            }
+
+            return ans;
+        }
+    }
+}
