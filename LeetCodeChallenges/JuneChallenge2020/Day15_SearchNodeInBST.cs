78601dafb1119d9180954cb92a0be4fa42a8d028
MonikaAherwar <monikaaherwar@gmail.com>
2020-06-15 13:16:04 +0530

    Day15: Search a Node In BST

---
+++ LeetCodeChallenges/JuneChallenge2020/Day15_SearchNodeInBST.cs
@@ -0,0 +1,33 @@
+﻿namespace LeetCodeChallenges.JuneChallenge2020
+{
+    class Day15_SearchNodeInBST
+    {
+        public TreeNode SearchBST(TreeNode root, int val)
+        {
+            while (root != null)
+            {
+                if (root.val == val)
+                {
+                    return root;
+                }
+
+                root = root.val > val ? root.left : root.right;
+            }
+
+            return null;
+        }
+    }
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+}
