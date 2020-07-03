348c7c59bccf931701045744eac1bb69cd88e0c5
MonikaAherwar <monikaaherwar@gmail.com>
2020-07-03 09:59:02 +0530

    Day01: Arranging Coins On Stairs Example: ¤ ¤ ¤ ¤ ¤ ¤

---
+++ LeetCodeChallenges/JulyChallenge2020/Day01_ArrangingCoinsOnStairs.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeChallenges.JulyChallenge2020
+{
+    class Day01_ArrangingCoinsOnStairs
+    {
+        var stairs = 0;
+            while(n >= (stairs+1)){
+            
+            n = n - (stairs + 1);
+            
+            if(n< 0){
+                break;
+            }
+
+            stairs++;
+        }
+               
+        return stairs;
+    }
+}
