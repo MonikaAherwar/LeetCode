7893d01c7794d44ad1fa7e0e1bcc21aa71102d97
MonikaAherwar <monikaaherwar@gmail.com>
2020-07-05 14:02:11 +0530

    Day05: Hamming Distance

---
+++ LeetCodeChallenges/JulyChallenge2020/Day05_HammingDistance.cs
@@ -0,0 +1,22 @@
+﻿namespace LeetCodeChallenges.JulyChallenge2020
+{
+    class Day05_HammingDistance
+    {
+        public int HammingDistance(int x, int y)
+        {
+
+            //Get XOR
+            int z = x ^ y;
+            int setBits = 0;
+
+            //Count number of bits which are set
+            while (z > 0)
+            {
+                setBits += z & 1;
+                z >>= 1;
+            }
+
+            return setBits;
+        }
+    }
+}
