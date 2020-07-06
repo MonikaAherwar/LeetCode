ea97b0010485fb668a8ad3a357b1e110a489993b
MonikaAherwar <monikaaherwar@gmail.com>
2020-07-06 20:42:09 +0530

    Day06: Plus One

---
+++ LeetCodeChallenges/JulyChallenge2020/Day06_PlusOne.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCodeChallenges.JulyChallenge2020
+{
+    class Day06_PlusOne
+    {
+        public int[] PlusOne(int[] digits)
+        {
+            var len = digits.Length;
+
+            for (var i = len - 1; i >= 0; i--)
+            {
+                if (digits[i] != 9)
+                {
+                    digits[i] = digits[i] + 1;
+                    return digits;
+                }
+                digits[i] = 0;
+            }
+
+            var result = new int[len + 1];
+            result[0] = 1;
+            return result;
+        }
+    }
+}
