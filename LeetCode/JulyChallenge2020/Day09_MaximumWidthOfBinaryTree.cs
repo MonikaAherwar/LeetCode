7ec56f2aed4213bc4e2a87694a647b1f6b1ce796
Monika Aherwar <monika.aherwar@factset.com>
2020-07-12 19:49:53 +0530

    Day09: Maximum Width Of Binary Tree

---
+++ LeetCode/JulyChallenge2020/Day09_MaximumWidthOfBinaryTree.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode.JulyChallenge2020
+{
+    class Day09_MaximumWidthOfBinaryTree
+    {
+
+        public int WidthOfBinaryTree(TreeNode root)
+        {
+            if (root == null) return 0;
+
+            var queue = new Queue<(TreeNode node, int index)>();
+            queue.Enqueue((root, 1));
+            int max_count = 1;
+
+            while (queue.Count > 0)
+            {
+                int size = queue.Count;
+                var first = 0;
+
+                for (int i = 0; i < size; i++)
+                {
+                    var item = queue.Dequeue();
+                    if (i == 0)
+                    {
+                        first = item.index;
+                    }
+                    else
+                    {
+                        max_count = Math.Max(max_count, item.index - first + 1);
+                    }
+
+                    if (item.node.left != null)
+                        queue.Enqueue((item.node.left, item.index * 2));
+
+                    if (item.node.right != null)
+                        queue.Enqueue((item.node.right, item.index * 2 + 1));
+                }
+            }
+
+            return max_count;
+        }
+    }
+}
