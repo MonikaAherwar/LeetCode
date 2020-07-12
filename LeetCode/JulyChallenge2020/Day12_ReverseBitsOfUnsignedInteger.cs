4308d11e3764115f7c778819dde06691f98fc41b
Monika Aherwar <monika.aherwar@factset.com>
2020-07-12 19:51:26 +0530

    Day12: Reverse Bits Of Unsigned Integer

---
+++ LeetCode/JulyChallenge2020/Day12_ReverseBitsOfUnsignedInteger.cs
@@ -0,0 +1,19 @@
+﻿namespace LeetCode.JulyChallenge2020
+{
+    class Day12_ReverseBitsOfUnsignedInteger
+    {
+        public uint reverseBits(uint n)
+        {
+
+            uint num = 0;
+            for (int i = 0; i < 32; ++i)
+            {
+                num <<= 1;
+                num += n % 2;
+                n >>= 1;
+            }
+
+            return num;
+        }
+    }
+}
