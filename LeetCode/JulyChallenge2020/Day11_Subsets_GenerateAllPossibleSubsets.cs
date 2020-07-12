687af7b5dafbc0045c63413a1e3d21cab58fa90d
Monika Aherwar <monika.aherwar@factset.com>
2020-07-12 19:50:57 +0530

    Day11: Subsets Problem - Generate All Possible Subsets

---
+++ LeetCode/JulyChallenge2020/Day11_Subsets_GenerateAllPossibleSubsets.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.JulyChallenge2020
+{
+    class Day11_Subsets_GenerateAllPossibleSubsets
+    {
+        public IList<IList<int>> Subsets(int[] nums)
+        {
+            return SubsetsHelper(nums, new List<IList<int>>(), 0);
+        }
+
+        public IList<IList<int>> SubsetsHelper(int[] nums, IList<IList<int>> result, int index)
+        {
+            if (index == 0)
+            {
+                result.Add(new List<int>());
+            }
+
+            if (index == nums.Length)
+            {
+                return result;
+            }
+
+            int precount = result.Count;
+            for (int i = 0; i < precount; i++)
+            {
+                var currentList = new List<int>(result[i]);
+                currentList.Add(nums[index]);
+                result.Add(currentList);
+            }
+
+            return SubsetsHelper(nums, result, index + 1);
+        }
+    }
+}
