c0d3690230eb78999b9d0fc5b724230a899e1543
Monika Aherwar <monikaaherwar@gmail.com>
2020-07-13 22:20:06 +0530

    Day13: Identical Tree

---
+++ LeetCode/JulyChallenge2020/Day13_IdenticalTree.cs
@@ -0,0 +1,18 @@
+﻿namespace LeetCode.JulyChallenge2020
+{
+    class Day13_IdenticalTree
+    {
+        public bool IsSameTree(TreeNode p, TreeNode q)
+        {
+            if (p == null || q == null)
+            {
+                return (p == q);
+            }
+
+            if (p.val != q.val)
+                return false;
+
+            return IsSameTree(p.left, q.left) && IsSameTree(p.right, q.right);
+        }
+    }
+}
