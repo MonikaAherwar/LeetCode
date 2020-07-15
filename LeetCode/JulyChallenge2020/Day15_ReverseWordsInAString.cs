32bcb736c823a030a0b5c522564221bd2e15756a
Monika Aherwar <monikaaherwar@gmail.com>
2020-07-16 03:55:10 +0530

    Day15: Reverse Words In A String

---
+++ LeetCode/JulyChallenge2020/Day15_ReverseWordsInAString.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace LeetCode.JulyChallenge2020
+{
+    class Day15_ReverseWordsInAString
+    {
+        public string ReverseWords(string s)
+        {
+            var stringArray = s.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            Array.Reverse(stringArray);
+            return string.Join(' ', stringArray);
+        }
+    }
+}
