08a8e65da4d66419fedbf6488300524ec8504ef9
Monika Aherwar <monikaaherwar@gmail.com>
2020-07-16 03:54:50 +0530

    Day14: Angle Between Hands Of A Clock

---
+++ LeetCode/JulyChallenge2020/Day14_AngleBetweenHandsOfAClock.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace LeetCode.JulyChallenge2020
+{
+    class Day14_AngleBetweenHandsOfAClock
+    {
+        public double AngleClock(int hour, int minutes)
+        {
+            const double anglePerHour = 360 / 12;
+            const double anglePerMinute = 360 / 60;
+
+            double minuteAngle = minutes * anglePerMinute;
+
+            double extraHourAngle = (minuteAngle / 360) * 30;
+
+            double hourAngle = (hour * anglePerHour) + extraHourAngle;
+
+            var angle = Math.Abs(minuteAngle - hourAngle);
+
+            return angle < 180 ? angle : (360 - angle);
+        }
+    }
+}
