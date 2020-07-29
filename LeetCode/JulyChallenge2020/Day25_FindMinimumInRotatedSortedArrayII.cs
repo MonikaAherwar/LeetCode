9cf52076d5b062d180f98c37e523f7c9b5fe407b
Monika Aherwar <monikaaherwar@gmail.com>
2020-07-29 20:10:01 +0530

    Day25: Find Minimum In Rotated Sorted Array II

---
+++ LeetCode/JulyChallenge2020/Day25_FindMinimumInRotatedSortedArrayII.cs
@@ -0,0 +1,17 @@
+﻿namespace LeetCode.JulyChallenge2020
+{
+    class Day25_FindMinimumInRotatedSortedArrayII
+    {
+        public int FindMin(int[] nums)
+        {
+            int len = nums.Length;
+            int i = 1;
+            while (i < len && nums[i - 1] <= nums[i])
+            {
+                i++;
+            }
+
+            return (i == len) ? nums[0] : nums[i];
+        }
+    }
+}
