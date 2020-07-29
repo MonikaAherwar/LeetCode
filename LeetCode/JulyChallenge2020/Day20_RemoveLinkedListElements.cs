5c4e9e5c5810ae51040fd7790e6aaf40e304ee1e
Monika Aherwar <monikaaherwar@gmail.com>
2020-07-29 20:09:19 +0530

    Day20: Remove Linked List Elements

---
+++ LeetCode/JulyChallenge2020/Day20_RemoveLinkedListElements.cs
@@ -0,0 +1,38 @@
+﻿using LeetCode.JuneChallenge2020;
+
+namespace LeetCode.JulyChallenge2020
+{
+    class Day20_RemoveLinkedListElements
+    {
+        public ListNode RemoveElements(ListNode head, int val)
+        {
+            var curr = head;
+            var prev = head;
+
+            while (curr != null)
+            {
+                if (curr.val == val)
+                {
+                    //remove head
+                    if (curr == head)
+                    {
+                        prev = curr = head = head.next;
+                    }
+                    else
+                    {
+                        //remove mid element
+                        prev.next = curr.next;
+                        curr = curr.next;
+                    }
+                }
+                else
+                {
+                    prev = curr;
+                    curr = curr.next;
+                }
+            }
+
+            return head;
+        }
+    }
+}
