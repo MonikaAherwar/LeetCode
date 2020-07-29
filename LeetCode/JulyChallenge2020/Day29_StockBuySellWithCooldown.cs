caf659538c4b127abaf13409306191fbdfc28d64
Monika Aherwar <monikaaherwar@gmail.com>
2020-07-29 20:11:03 +0530

    Day29: Stock Buy Sell With Cooldown

---
+++ LeetCode/JulyChallenge2020/Day29_StockBuySellWithCooldown.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace LeetCode.JulyChallenge2020
+{
+    class Day29_StockBuySellWithCooldown
+    {
+        public int MaxProfit(int[] prices)
+        {
+            var len = prices.Length;
+            var result = new int[len];
+            var sell = new int[len];
+
+            for (int i = 1; i < len; i++)
+            {
+                var diff = prices[i] - prices[i - 1];
+                sell[i] = diff + Math.Max(sell[i - 1], i > 2 ? result[i - 3] : 0);
+                result[i] = Math.Max(sell[i], result[i - 1]);
+            }
+
+            return len == 0 ? 0 : result[len - 1];
+        }
+    }
+}
