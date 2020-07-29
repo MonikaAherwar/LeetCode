e0153598e313db9a39cee9cc365d3defd39a29c2
Monika Aherwar <monikaaherwar@gmail.com>
2020-07-29 20:09:40 +0530

    Day23: Single Number III

---
+++ LeetCode/JulyChallenge2020/Day23_SingleNumberIII.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;;
+
+namespace LeetCode.JulyChallenge2020
+{
+    class Day23_SingleNumberIII
+    {
+        public int[] SingleNumber(int[] nums)
+        {
+            var hashSet = new HashSet<int>();
+            foreach (var n in nums)
+            {
+                if (!hashSet.Contains(n))
+                {
+                    hashSet.Add(n);
+                }
+                else
+                {
+                    hashSet.Remove(n);
+                }
+            }
+
+            int[] result = new int[2];
+            hashSet.CopyTo(result);
+            return result;
+        }
+    }
+}
