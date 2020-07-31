fb9d6bd3b9480a268e9867d1963cb79c140f33db
Monika Aherwar <monikaaherwar@gmail.com>
2020-08-01 01:27:55 +0530

    Day31: Climb Stairs

---
+++ LeetCode/JulyChallenge2020/Day31_ClimbStairs.cs
@@ -0,0 +1,31 @@
+﻿namespace LeetCode.JulyChallenge2020
+{
+    class Day31_ClimbStairs
+    {
+        public int ClimbStairs(int n)
+        {
+            //This solution runs into TLE
+            //if(n <= 2)
+            //{
+            //    return n;
+            //}
+            //return ClimbStairs(n-1) + ClimbStairs(n-2);
+
+            if (n <= 2)
+            {
+                return n;
+            }
+
+            var dp = new int[n];
+            dp[0] = 1;
+            dp[1] = 2;
+
+            for (int i = 2; i < n; i++)
+            {
+                dp[i] = dp[i - 1] + dp[i - 2];
+            }
+
+            return dp[n - 1];
+        }
+    }
+}
