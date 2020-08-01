f25a5d4131974a3a5d131701b349212edfbadc7f
Monika Aherwar <monikaaherwar@gmail.com>
2020-08-02 02:38:01 +0530

    Day01: Detect Capital Word

---
+++ LeetCode/AugustChallenge2020/Day01_DetectCapitalWord.cs
@@ -0,0 +1,18 @@
+﻿namespace LeetCode.AugustChallenge2020
+{
+    class Day01_DetectCapitalWord
+    {
+        public bool DetectCapitalUse(string word)
+        {
+            if (word == "")
+            {
+                return true;
+            }
+
+            bool allUpperCase = word == word.ToUpper();
+            bool substringAfterFirstLetterIsLowerCase = word.Substring(1) == word.Substring(1).ToLower();
+
+            return (allUpperCase || substringAfterFirstLetterIsLowerCase);
+        }
+    }
+}
