31dfb0b62bf0489227266bad898393cd4ca9520e
Monika Aherwar <monikaaherwar@gmail.com>
2020-08-04 00:34:18 +0530

    Day03: Is Palindrome After Ignoring Special Characters

---
+++ LeetCode/AugustChallenge2020/Day03_IsPalindromeAfterIgnoringSpecialCharacters.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace LeetCode.AugustChallenge2020
+{
+    class Day03_IsPalindromeAfterIgnoringSpecialCharacters
+    {
+        public bool IsPalindrome(string s)
+        {
+            int i = 0;
+            int j = s.Length - 1;
+
+            while (i < j)
+            {
+                if (!Char.IsLetterOrDigit(s[i]))
+                {
+                    i++;
+                }
+                else if (!Char.IsLetterOrDigit(s[j]))
+                {
+                    j--;
+                }
+                else if (Char.ToUpperInvariant(s[i]) == char.ToUpperInvariant(s[j]))
+                {
+                    i++;
+                    j--;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
