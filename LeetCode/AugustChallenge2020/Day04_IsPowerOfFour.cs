df136234771186558a1320bc1b2d02b2f15bdffa
Monika Aherwar <monikaaherwar@gmail.com>
2020-08-04 22:10:25 +0530

    Day04: Is Power Of Four Without Loop

---
+++ LeetCode/AugustChallenge2020/Day04_IsPowerOfFour.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace LeetCode.AugustChallenge2020
+{
+    class Day04_IsPowerOfFour
+    {
+        public bool IsPowerOfFour(int num)
+        {
+
+            if (num <= 0)
+            {
+                return false;
+            }
+
+            var b = (int)Math.Log(num, 4);
+            return Math.Pow(4, b) == num;
+        }
+    }
+}
